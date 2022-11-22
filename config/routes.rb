aaa05aaa558fee19f5783f7864ac61faeaf22638
Tony <mutsuoara@gmail.com>
2022-11-21 22:52:12 -0800

    added about us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'contact', to: 'pages#contact'
+  get 'aboutus', to: 'pages#aboutus'
+
+
 end
