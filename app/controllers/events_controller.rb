026e389939fa68a2d385936a01d17099b5bf76cb
Tony <mutsuoara@gmail.com>
2022-12-06 00:46:57 -0800

    added events sections; create, form, and new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,35 @@
 class EventsController < ApplicationController
+before_action :set_event, only: %i[show]
+    
     def index
         @events = Event.all
+    end
+
+    def show
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.create(event_params)
+        respond_to do |format|
+            if @event.save
+                format.html {redirect_to event_url(@event), notice: "Event was created successfully"}
+            else
+                format.html {render :new, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    private
+
+    def set_event
+        @event = Event.find(params[:id])
+    end
+
+    def event_params
+        params.require(:event).permit(:title, :content)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   has_rich_text :content
+  belongs_to :user
 end
